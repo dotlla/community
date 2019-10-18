93164d61440509bc0c48d66f0be59b708f5baab6
dotlla <dotlla@163.com>
2019-10-18 22:35:44 +0800

    init repo

---
+++ src/main/java/life/majing/community/commnity/CommnityApplication.java
@@ -0,0 +1,13 @@
+package life.majing.community.commnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommnityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/majing/community/commnity/CommnityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.majing.community.commnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
