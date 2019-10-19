9c0132fdfadf7bc28585250a96edd39c20ce13da
dotlla <dotlla@163.com>
2019-10-19 23:16:19 +0800

    add index and bootstrap

---
+++ src/main/java/life/majing/community/commnity/controller/HelloController.java
@@ -0,0 +1,13 @@
+package life.majing.community.commnity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String index(){ return "index";
+    }
+}
